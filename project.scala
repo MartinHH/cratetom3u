00ed81da21fe011e71ee78fe4c74e7f254332e59
Martin Hansen <code@martin-hansen.de>
2023-11-05 11:26:53 +0100

    chore: bump scallop version

--- project.scala
+++ project.scala
@@ -1,6 +1,6 @@
 //> using scala "3.3.1"
 //> using platform "jvm"
-//> using lib "org.rogach::scallop::4.1.0"
+//> using lib "org.rogach::scallop::5.0.1"
 //> using jvm "17"
 //> using buildInfo
 //> using computeVersion git:tag
